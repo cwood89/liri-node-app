15dd69e40921f71cff2d24d01ecce691c66025c2
woodtop <cwood89@mac.com>
2018-12-04 18:55:14 -0500

    finished spotify search

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+// \n=new line - use to limit amount of console logs
+
 require("dotenv").config();
 var chalk = require("chalk")
 var moment = require("moment");
@@ -6,16 +8,16 @@ var key = require("./keys.js");
 var fs = require("fs");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(key.spotify);
-const { g, b, gr, r, y } = require("./console");
 var search = process.argv.slice(3).join(" ");
 var searchC = process.argv.slice(3).join("");
 var input = process.argv[2];
 var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
 
-// spotify = spotify-this-(song, artist)
+
 switch (input) {
+    // spotify song search===========================================================================
     case "spotify-this-song":
-        spotify.search({ type: 'track', query: search, limit: 2 }, function (err, data) {
+        spotify.search({ type: 'track', query: search, limit: 1 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
@@ -23,13 +25,34 @@ switch (input) {
             console.log(data.tracks.items[0].name)
             console.log(chalk.redBright.bold("Artist:"));
             console.log(data.tracks.items[0].artists[0].name);
-            console.log(chalk.redBright.bold("Preview:"));
-            console.log()
+            console.log(chalk.redBright.bold("Spotify Link:"));
+            console.log(data.tracks.items[0].external_urls.spotify);
+            console.log(chalk.redBright.bold("Album:"));
+            console.log(data.tracks.items[0].album.name);
         });
         break;
+    // spotify artist search========================================================================
     case "spotify-this-artist":
-
+        spotify.search({ type: 'artist', query: search, limit: 5 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(chalk.redBright.bold("Artist:"))
+            console.log(data.artists.items[0].name);
+            console.log(chalk.redBright.bold("Genre:"))
+            console.log(data.artists.items[0].genres[0]);
+        });
+        spotify.search({ type: 'track', query: search, limit: 5 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(chalk.redBright.bold("Top Songs:"))
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                console.log(data.tracks.items[i].name)
+            }
+        });
         break;
+    // imdb movie search =======================================================================
     case "movie-this":
         axios.get(queryUrl).then(
             function (response) {
@@ -51,6 +74,7 @@ switch (input) {
                 console.log(response.data.Actors);
             })
         break;
+    // bands in town search=========================================================================
     case "concert-this":
         axios.get("https://rest.bandsintown.com/artists/" + searchC + '/events?app_id=codingbootcamp')
             .then(function (response) {
@@ -63,10 +87,8 @@ switch (input) {
                 console.log(chalk.redBright.bold("Location:"));
                 console.log(response.data[0].venue.city + ", " + response.data[0].venue.region)
             })
-
+        break;
+    // do what it says=============================================================================
+    case "do-what-it-says":
 
 }
-
-
-// bands = axios concert-this
-// movie = axios movie-this
